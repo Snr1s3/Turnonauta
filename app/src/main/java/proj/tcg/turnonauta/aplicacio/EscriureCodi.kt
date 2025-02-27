4d0f5a867860e32b18ad821dec8c6e68ae553319
Alba Segura <2023_alba.segura@gmail.com>
2025-02-27 19:46:26 +0100

    Torneig preview i recycler view

--- app/src/main/java/proj/tcg/turnonauta/aplicacio/EscriureCodi.kt
+++ app/src/main/java/proj/tcg/turnonauta/aplicacio/EscriureCodi.kt
@@ -12,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsCompat
 import proj.tcg.turnonauta.R
 
+import proj.tcg.turnonauta.screen.MenuInferiorAndroid
+
 class EscriureCodi : AppCompatActivity() {
 
     private lateinit var bTorneig : Button
@@ -20,7 +22,9 @@ class EscriureCodi : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_escriure_codi)
-        hideSystemNavigationBar()
+        val menuInferior = MenuInferiorAndroid(window)
+        menuInferior.hideSystemNavigationBar()
+
         bTorneig = findViewById(R.id.bTorneig)
 
         bTorneig.setOnClickListener {
@@ -29,33 +33,5 @@ class EscriureCodi : AppCompatActivity() {
         }
     }
 
-    private fun hideSystemNavigationBar() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val windowInsetsController = window.insetsController ?: return
-            windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars())
-            windowInsetsController.systemBarsBehavior =
-                WindowInsetsController.BEHAVIOR_DEFAULT
-        } else {
-            val decorView = window.decorView
-            decorView.systemUiVisibility = (
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    )
-        }
-    }
 
-    private fun showSystemNavigationBar() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val windowInsetsController = window.insetsController ?: return
-            windowInsetsController.show(WindowInsetsCompat.Type.navigationBars())
-        } else {
-            val decorView = window.decorView
-            decorView.systemUiVisibility = (
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    )
-        }
-    }
 }
