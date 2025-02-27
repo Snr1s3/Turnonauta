21c4c9e7c64b5be15e5e4713321232862d22506f
Alba Segura <2023_alba.segura@gmail.com>
2025-02-27 16:35:34 +0100

    Torneig preview i recycler view

--- app/src/main/java/proj/tcg/turnonauta/aplicacio/EscriureCodi.kt
+++ app/src/main/java/proj/tcg/turnonauta/aplicacio/EscriureCodi.kt
@@ -1,22 +1,32 @@
 package proj.tcg.turnonauta.aplicacio
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.WindowInsetsController
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import proj.tcg.turnonauta.R
 
 class EscriureCodi : AppCompatActivity() {
+
+    private lateinit var bTorneig : Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_escriure_codi)
         hideSystemNavigationBar()
+        bTorneig = findViewById(R.id.bTorneig)
+
+        bTorneig.setOnClickListener {
+            val intent = Intent(this, PreviewTorneig::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun hideSystemNavigationBar() {
