434cbf4ed4cde9e13f04dd8379e77e943a3f0dc6
PauChacon <2023_pau.chacon@iticbcn.cat>
2025-04-03 15:34:34 +0200

    modo home y mas

--- app/src/main/java/proj/tcg/turnonauta/config/Configuracio.kt
+++ app/src/main/java/proj/tcg/turnonauta/config/Configuracio.kt
@@ -15,9 +15,17 @@ class Configuracio : AppCompatActivity() {
     private lateinit var botonPerfil: MaterialButton
     private lateinit var botonLogOut: MaterialButton
     private lateinit var botonModo: ImageButton
+    private lateinit var botoPoliticas: MaterialButton
+    private lateinit var botoTerms: MaterialButton
+    private lateinit var botoUbi: ImageButton
+
+
+
 
     private var isRedBorder = false // Estado inicial (borde verde)
     private var isNight=false
+    private var isUbiON=false
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,22 +37,27 @@ class Configuracio : AppCompatActivity() {
         botonPerfil = findViewById(R.id.btnPerfil)
         botonLogOut=findViewById(R.id.btnLogOut)
         botonModo=findViewById(R.id.btnModo)
+        botoPoliticas=findViewById(R.id.btnPoliticas)
+        botoTerms=findViewById(R.id.btnTerms)
+        botoUbi=findViewById(R.id.btn_ubi)
 
 
         botonImagen.setImageResource(R.drawable.campana_noti_on)
         botonImagen.setBackgroundResource(R.drawable.rounded_green_border_button) // Borde verde
 
         botonModo.setImageResource(R.drawable.sol_turno)
-        botonModo.setImageResource(R.drawable.rounded_day_button)
+        botonModo.setBackgroundResource(R.drawable.rounded_day_button)
+
+        botoUbi.setBackgroundResource(R.drawable.rounded_green_border_button)
+        botoUbi.setImageResource(R.drawable.location_on)
 
         botonModo.setOnClickListener {
             if(isNight) {
-
-                botonModo.setImageResource(R.drawable.luna_turno)
-                botonModo.setImageResource(R.drawable.rounded_night_button)
-            }else{
                 botonModo.setImageResource(R.drawable.sol_turno)
-                botonModo.setImageResource(R.drawable.rounded_day_button)
+                botonModo.setBackgroundResource(R.drawable.rounded_day_button)
+            }else{
+                botonModo.setImageResource(R.drawable.luna_turno)
+                botonModo.setBackgroundResource(R.drawable.rounded_night_button)
             }
             isNight=!isNight
 
@@ -61,12 +74,33 @@ class Configuracio : AppCompatActivity() {
             isRedBorder = !isRedBorder // Alternar estado
         }
 
+
+        botoUbi.setOnClickListener {
+            if (isUbiON) {
+                botoUbi.setImageResource(R.drawable.location_on)
+                botoUbi.setBackgroundResource(R.drawable.rounded_green_border_button)
+            } else {
+                botoUbi.setImageResource(R.drawable.location_off)
+                botoUbi.setBackgroundResource(R.drawable.rounded_red_border_button)
+            }
+            isUbiON = !isUbiON
+        }
+
+
         botonPerfil.setOnClickListener {
             val intent = Intent(this, EditarPerfil::class.java)
             startActivity(intent)
         }
         botonLogOut.setOnClickListener{
             val intent= Intent(this,PantallaLogin::class.java)
+            startActivity(intent)
+        }
+        botoTerms.setOnClickListener{
+            val intent= Intent(this,TermsOfUse::class.java)
+            startActivity(intent)
+        }
+        botonLogOut.setOnClickListener{
+            val intent= Intent(this,PoliticasPrivacidad::class.java)
             startActivity(intent)
         }
     }
